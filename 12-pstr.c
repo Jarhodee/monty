63cddaed2eb0e3152ec4faf531cfb0b35637aff7
GaReN <christopherdezmond@gmail.com>
2023-08-24 23:30:14 +0100

    monty

---
+++ 12-pstr.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+void f_pstr(stack_t **head, unsigned int counters)
+{
+	stack_t *h;
+	(void)counters;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
---
+++ 5-nop.c
@@ -0,0 +1,7 @@
+#include "monty.h"
+
+void f_nop(stack_t **head, unsigned int counters)
+{
+	(void) counters;
+	(void) head;
+}
